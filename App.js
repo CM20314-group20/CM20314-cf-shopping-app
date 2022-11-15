8f787543b05228f84c0b026e3ff33cb22dfb6afd
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2022-11-15 20:03:32 +0000

    Reactive text start

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  
+  const [update, setUpdate] = useState("")
   return (
+    <>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter Text Below:</Text>
+      <TextInput placeholder="Enter" onChangeText={e => setUpdate(e)}></TextInput>
+      <Text style={{color: '#F00'}}>Below is real time update:</Text>
+      <Text>{update}</Text>
       <StatusBar style="auto" />
     </View>
+    </>
   );
 }
 
