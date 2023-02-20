20b4b8c7f02ee94c1c4281e749c5fafd6f28cf77
Jia Sheng Liu <jialiu505@gmail.com>
2023-02-20 12:20:02 +0000

    added graph

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,64 @@
-import React, {useState} from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 
 export default function Home() {
-  
   const [update, setUpdate] = useState("")
   return (
     <>
-    <View style={styles.container}>
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.graph} >History</Text>
+        <LineChart
+          data={{
+            labels: ["January", "February", "March", "April", "May", "June"],
+            datasets: [
+              {
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100
+                ]
+              }
+            ]
+          }}
+          width={Dimensions.get("window").width} // from react-native
+          height={220}
+          yAxisLabel=""
+          yAxisSuffix="kg"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: "red",
+            backgroundGradientFrom: "red",
+            backgroundGradientTo: "green",
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 2) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 2) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16
+            },
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "white"
+            }
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16
+          }}
+        />
+      </View>
     </>
   );
 }
@@ -17,6 +68,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'top',
   },
+  graph: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 6,
+    paddingHorizontal: 18,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'green',
+    shadowColor: 'darkgrey',
+    shadowOpacity: 1.5,
+    elevation: 8,
+    shadowRadius: 5,
+    shadowOffset: { width: 1, height: 5 },
+    marginVertical: 8
+  }
 });
