00764c8243148dc4d88305f693869e7657055354
Joshua Peachey <jp2571@bath.ac.uk>
2023-02-20 11:58:42 +0000

    Camera Button fixed

--- src/components/CameraButton.js
+++ src/components/CameraButton.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 
-export default function Button({title, onPress, icon, color}) {
+export default function CameraButton({title, onPress, icon, color}) {
   return (
     <TouchableOpacity onPress={onPress} style={styles.button}>
     <Entypo name={icon} size={28} color={color ? color: '#f1f1f1'}/>
