995734bac7d300c27b6b3a4da504be5ebb3b2287
Jia Sheng Liu <jialiu505@gmail.com>
2023-03-02 17:38:00 +0000

    change little things

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -12,13 +12,16 @@ import {
 
 export default function Home() {
   // const [update, setUpdate] = useState("")
+  const Item = ({ item }) => {
+    return <View style={styles.item}>{item.icon}</View>;
+  };
   return (
     <>
       <View style={styles.container}>
         <Text style={styles.header01} >History</Text>
         <LineChart
           data={{
-            labels: ["January", "February", "March", "April", "May", "June"],
+            labels: ["December", "January", "February", "March", "April", "May"],
             datasets: [
               {
                 data: [
@@ -38,37 +41,39 @@ export default function Home() {
           yAxisSuffix="kg"
           yAxisInterval={1} // optional, defaults to 1
           chartConfig={{
-            backgroundColor: "red",
+            backgroundColor: "blue",
             backgroundGradientFrom: "red",
             backgroundGradientTo: "green",
-            decimalPlaces: 2, // optional, defaults to 2dp
+            decimalPlaces: 0, // optional, defaults to 2dp
             color: (opacity = 2) => `rgba(255, 255, 255, ${opacity})`,
             labelColor: (opacity = 2) => `rgba(255, 255, 255, ${opacity})`,
             style: {
               borderRadius: 16
             },
             propsForDots: {
-              r: "6",
+              r: "3",
               strokeWidth: "2",
-              stroke: "white"
+              stroke: "black"
             }
           }}
           bezier
           style={{
             marginVertical: 8,
-            borderRadius: 16
+            // borderRadius: 16
+            borderRadius: 30
           }}
         />
         <Text style={styles.results} >1.6 miles of CO2 reduced</Text>
         <Text style={styles.results} >1kw energy saved</Text>
         <Text style={styles.results} >37 fishs saved</Text>
+        <Text style={styles.results} >10 trees planeted</Text>
         <Text style={styles.header01} > Achievements </Text>
-        <Text style={styles.results} >Star Reducer:</Text>
+        <Text style={styles.Achievements} >Star Reducer:</Text>
         <Text>CF lowered by more than 50 kg from previous month</Text>
         <View style={styles.goal}>
           <Button
             title="GOALS"
-            color="green"
+            color="white"
             height="30"
             // fontSize="30px"
             onPress={() => alert('To save 10kg')} />
@@ -106,6 +111,7 @@ const styles = StyleSheet.create({
     marginVertical: 8
   },
   results: {
+    fontSize: '18px',
     alignItems: 'center',
     justifyContent: 'center',
     paddingVertical: 6,
@@ -114,7 +120,7 @@ const styles = StyleSheet.create({
     elevation: 3,
     // backgroundColor: 'green',
     // shadowColor: 'darkgrey',
-    color: 'darkgreen',
+    color: 'black',
     elevation: 8,
     marginVertical: 8
   },
@@ -131,14 +137,44 @@ const styles = StyleSheet.create({
 
     marginVertical: 8
   },
+  Achievements: {
+    fontSize: '20px',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    alignContent: 'left',
+    color: 'orange',
+    paddingVertical: 1,
+    paddingHorizontal: 18,
+    borderRadius: 4,
+    elevation: 8,
+    // textDecorationLine: "underline",
+
+    marginVertical: 8
+  },
   goal: {
     alignItems: 'left',
     paddingVertical: 1,
     paddingHorizontal: 18,
-    // backgroundColor: 'red',
+    backgroundColor: 'darkgreen',
     borderRadius: 4,
     elevation: 3,
     elevation: 8,
     marginVertical: 10
+  },
+  app: {
+    flex: 4, // the number of columns you want to devide the screen into
+    marginHorizontal: "auto",
+    width: 400
+  },
+  item: {
+    flex: 1,
+    maxWidth: "25%", // 100% devided by the number of rows you want
+    alignItems: "center",
+
+    // my visual styles; not important for the grid
+    padding: 10,
+    backgroundColor: "rgba(249, 180, 45, 0.25)",
+    borderWidth: 1.5,
+    borderColor: "#fff"
   }
 });
