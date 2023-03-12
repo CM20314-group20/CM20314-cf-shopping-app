6867ddb4be03016c5bb254ea6d700924926ce9fe
Harvey A <harvey7ayling@gmail.com>
2023-03-12 17:07:13 +0000

    ESG20-113: Connecting backend functionalities.

--- backend/product_data_backend/Products.py
+++ backend/product_data_backend/Products.py
@@ -5,8 +5,7 @@ from .DB_Interface import get_cf_from_category
 
 
 # Dictionary keys of neccesary information. More can be added if required.
-PRODUCT_KEYS : Final = ['_id', 'product_name', 'ecoscore_grade', 'ecoscore_score', '_keywords']
-CO2_KEYS : Final = ['co2_agriculture', 'co2_consumption', 'co2_distribution', 'co2_packaging', 'co2_processing', 'co2_transportation', 'co2_total']
+PRODUCT_KEYS : Final = ['product_name']
 
 # Helper guard function to process requests which could return errors.
 def _try_request(function: Callable, args: dict) -> Any:
@@ -24,8 +23,9 @@ def _process_product_dict(product: dict) -> dict:
     
     # If Ecoscore data is avaliable, proccess additional CO2 information.
     if product['ecoscore_grade'] not in ['not-applicable', 'unknown']:
-        new_dict['co2_data'] = {k : product['ecoscore_data']['agribalyse'][k] for k in CO2_KEYS}
         new_dict['category'] = product['ecoscore_data']['agribalyse']['name_en']
+        new_dict['co2_total'] = product['ecoscore_data']['agribalyse']['co2_total']
+
 
     return new_dict
 
@@ -39,28 +39,31 @@ def _get_most_freq_category(product_name: str) -> list:
     for product in products:
         if 'categories' in product.keys():
             frequencies.add(product['categories'])
+            
     return frequencies.get_most_freq()
 
 class ProductData:
     # Get product information from name.
     @staticmethod
     def product_from_name(product_name: str) -> dict:
-        response = _try_request(openfoodfacts.products.search, {'query': product_name})
-        num_pages = response['page_count']
+    # Search results for a product.
+        products = _try_request(openfoodfacts.products.search, {'query': product_name})['products']
 
-        # Search results for a product.
-        for page in range(1, num_pages+1):
-            products = _try_request(openfoodfacts.products.search, {'query': product_name, 'page' : page})['products']
-
-            for product in products:
-                if product['ecoscore_grade'] not in ['not-applicable', 'unknown']:
-                    return _process_product_dict(product)
+        for product in products:
+            if product['ecoscore_grade'] not in ['not-applicable', 'unknown']:
+                print(product_name, _process_product_dict(product))
+                return _process_product_dict(product)
 
         # If close match cannot be found, revert to category database
         category = _get_most_freq_category(product_name)
+        if category == None:
+            category = product_name
+        
+        category, co2_data = get_cf_from_category(category)
+            
         product = {'product_name' : product_name,
                    'category': category,
-                   'co2_data' : {'co2_total_per_kg' : get_cf_from_category(category)}}
+                   'co2_total_per_kg' : co2_data}
         return product
 
     # Get product information from barcode. Will return products with no eco data avaliable.
